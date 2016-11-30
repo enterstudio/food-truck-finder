59539a23d41e0669f9f04afe012497043cd60d70
William Pelrine <drawncloser@gmail.com>
2016-11-30 15:54:12 -0600

    Hook up new page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { Auth } from '@ionic/cloud-angular';
-import { NavController, Platform } from 'ionic-angular';
+import { Auth, Database } from '@ionic/cloud-angular';
+import { NavController, Platform, ModalController } from 'ionic-angular';
 import { 
   GoogleMap, 
   GoogleMapsEvent, 
@@ -9,14 +9,29 @@ import {
   GoogleMapsMarkerOptions,
   CameraPosition
 } from 'ionic-native';
+import { NewPage } from '../new/new';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private map: GoogleMap;
 
-  constructor(public platform: Platform, public navCtrl: NavController, public auth: Auth) {}
+  constructor(
+    public platform: Platform,
+    public navCtrl: NavController,
+    public auth: Auth,
+    public db: Database,
+    public modalCtrl: ModalController
+  ) {
+    this.db.connect();
+    this.db.collection('locations').watch().subscribe((locations) => {
+      console.log(locations);
+    }, (error) => {
+      console.error(error);
+    })
+  }
 
   ngAfterViewInit() {
     this.platform.ready().then(() => {
@@ -26,10 +41,10 @@ export class HomePage {
 
   loadMap() {
     let element: HTMLElement = document.getElementById('map');
-    let map = new GoogleMap(element);
+    this.map = new GoogleMap(element);
 
-    map.one(GoogleMapsEvent.MAP_READY).then(() => {
-      map.setClickable(false);
+    this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
+      this.setClickable(false);
       let ionic: GoogleMapsLatLng = new GoogleMapsLatLng(43.0741904,-89.3809802);
       let position: CameraPosition = {
         target: ionic,
@@ -37,17 +52,38 @@ export class HomePage {
         tilt: 30
       };
 
-      map.moveCamera(position);
+      this.map.moveCamera(position);
 
       let markerOptions: GoogleMapsMarkerOptions = {
         position: ionic,
-        title: 'Ionic'
+        title: 'Ionic',
+        snippet: 'Ionic is the beautiful, free and open source mobile SDK for developing native and progressive web apps with ease.'
       };
 
-      map.addMarker(markerOptions).then((marker: GoogleMapsMarker) => {
-        marker.showInfoWindow();
-      });
+      this.map.addMarker(markerOptions);
+    });
+
+    this.map.on(GoogleMapsEvent.MAP_CLICK).subscribe((event) => {
+      this.openCreateModal(event.lat, event.lng);
+    }, (error) => {
+      console.error(error);
+    });
+
+    this.map.on(GoogleMapsEvent.MARKER_CLICK).subscribe((event) => {
+      console.log(event);
+    }, (error) => {
+      console.error(error);
     })
+  }
+
+  setClickable(clickable: boolean) {
+    this.map.setClickable(clickable);
+  }
+
+  openCreateModal(lat: number, lng: number) {
+    this.setClickable(false);
+    let modal = this.modalCtrl.create(NewPage, {lat: lat, lng: lng});
+    modal.present();
   }
 
   logout() {
